8cc016d518448c08c31422f855ae25e37e1a38f4
jothidharman <jothi.dharman95@gmail.com>
2022-10-18 15:06:38 +0530

    New project added

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,38 @@
+package org.emp;
+
+public class Employee {
+	
+	public void empId() {
+		System.out.println("1303641");
+	}
+	
+	public void empName() {
+		System.out.println("Jothi D");
+	}
+	
+	public void empDob() {
+		System.out.println("21-05-1995");
+	}
+	
+	public void empPhone() {
+		System.out.println("0123456789");
+	}
+	
+	public void empEmail() {
+		System.out.println("jothi.dharman95@gmail.com");
+	}
+	
+	public void empAddress() {
+		System.out.println("Chennai, TN");
+	}
+
+	public static void main(String[] args) {
+		Employee ee = new Employee();
+		ee.empId();
+		ee.empName();
+		ee.empDob();
+		ee.empPhone();
+		ee.empEmail();
+		ee.empAddress();
+	}
+}
